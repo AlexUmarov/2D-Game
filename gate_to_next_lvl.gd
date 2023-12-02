badfafce346c6c6a79d78cd680786835a889dfb1
Alex <asocil@mail.ru>
2023-12-02 16:27:31 +0300

    22
    222

--- gate_to_next_lvl.gd
+++ gate_to_next_lvl.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
 @export var next_level: int
+@onready var audio = $AudioStreamPlayer2D
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
 		if next_level == 2:
+			audio.play()
+			audio.finished
 			get_tree().change_scene_to_file("res://level_02/level_02.tscn")
 		if next_level == 3:
+			audio.play()
+			audio.finished
 			get_tree().change_scene_to_file("res://level_03/level_03.tscn")
---
+++ level_03/items/light_star.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var move_direction: Vector2
+var energy: float
+var wander_time: float
+var move_speed := 10.0
+@onready var light = $PointLight2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if wander_time > 0:
+		wander_time -= delta
+	else:
+		randomize_wander()
+	light.energy = wander_time
+	#position = move_direction * move_speed
+
+
+func randomize_wander():
+	move_direction = Vector2(randf_range(-.1, .1), 0).normalized()
+	wander_time = randf_range(.5, .7)
+	#energy = randf_range(.1, .3)

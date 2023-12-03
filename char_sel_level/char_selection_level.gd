b84663b0eb7e8e2ae0236387f4021c524d39fd17
Alex <asocil@mail.ru>
2023-12-03 16:19:26 +0300

    333
    333

--- char_sel_level/char_selection_level.gd
+++ char_sel_level/char_selection_level.gd
@@ -36,3 +36,7 @@ func _on_button_pressed():
 		print("load scene ", current_character_selecterd)
 		Global.set_player_character(current_character_selecterd)
 		get_tree().change_scene_to_file("res://level_03/level_03.tscn")
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
--- menu.gd
+++ menu.gd
@@ -4,5 +4,5 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://level_01/level_01.tscn")
+	get_tree().change_scene_to_file("res://char_sel_level/char_selection_level.tscn")
 
